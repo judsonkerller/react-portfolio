3df57076c1c6de05eda8e3aad4659b70761959d4
judsonkerller <judsonkerller@gmail.com>
2024-03-11 22:33:48 -0300

    fix: update imports from swiper library

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -8,8 +8,8 @@ import { Pagination } from 'swiper';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-import 'swiper/css';
-import 'swiper/css/pagination';
+import 'swiper/swiper.min.css'
+import 'swiper/modules/pagination/pagination.min.css'
 
 const data = [
   {
