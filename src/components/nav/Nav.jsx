a260c00d62020f1c3e960431dae6b6d80e6283b3
judsonkerller <judsonkerller@gmail.com>
2024-03-14 21:16:09 -0300

    chore: update navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -6,6 +6,7 @@ import './nav.css'
 import { RiServiceLine } from 'react-icons/ri' 
 import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai' 
 import { BiBook, BiMessageSquareDetail } from 'react-icons/bi' 
+import { MdOutlineWorkOutline } from "react-icons/md";
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
@@ -14,7 +15,7 @@ const Nav = () => {
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
-      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><MdOutlineWorkOutline /></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
     </nav>
   )
