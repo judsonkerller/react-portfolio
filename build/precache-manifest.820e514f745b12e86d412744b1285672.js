ec7b3ba5228472174b46d4508fe1dbd1257dad8a
judsonkerller <judsonkerller@gmail.com>
2024-03-14 21:31:01 -0300

    chore: update build folder

---
+++ build/precache-manifest.820e514f745b12e86d412744b1285672.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9156bde554ac9680936",
+    "url": "/static/css/main.60713704.chunk.css"
+  },
+  {
+    "revision": "a9156bde554ac9680936",
+    "url": "/static/js/main.e37ca7a2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "907531a768e9b5e6dd01",
+    "url": "/static/css/2.1b0361c5.chunk.css"
+  },
+  {
+    "revision": "907531a768e9b5e6dd01",
+    "url": "/static/js/2.9169d40e.chunk.js"
+  },
+  {
+    "revision": "c825873e49f9b96dd00652f0dafeb140",
+    "url": "/static/media/me.c825873e.png"
+  },
+  {
+    "revision": "f83b8f521d32511ede0254f9662faed8",
+    "url": "/static/media/cv.f83b8f52.pdf"
+  },
+  {
+    "revision": "6b9f572d2f57115f6eba3aa4d17aa36a",
+    "url": "/static/media/me-about.6b9f572d.jpg"
+  },
+  {
+    "revision": "cf45ab6cb3ac31b8a28296f2941ad849",
+    "url": "/static/media/groppa.cf45ab6c.png"
+  },
+  {
+    "revision": "8de31f6ad1c9e4b8a8cace5de57bc59f",
+    "url": "/static/media/plantmanager.8de31f6a.png"
+  },
+  {
+    "revision": "edc8f1a898d65479e185e05877faddbc",
+    "url": "/static/media/event-platform.edc8f1a8.png"
+  },
+  {
+    "revision": "9984485e55dca654d41015f0cdac4ec8",
+    "url": "/index.html"
+  }
+];
