a9edd386132c698530b652d6ae38edf813f9d668
judsonkerller <judsonkerller@gmail.com>
2024-03-14 21:15:48 -0300

    chore: update portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,30 +1,33 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../assets/portfolio1.jpg'
-import IMG2 from '../../assets/portfolio2.jpg'
-import IMG3 from '../../assets/portfolio3.jpg'
+import IMG1 from '../../assets/groppa.png'
+import IMG2 from '../../assets/plantmanager.png'
+import IMG3 from '../../assets/event-platform.png'
 
 const data = [
   {
     id: 1,
     image: IMG1,
-    title: 'Crypto Currency Dashboard & Financial Visualization',
+    title: 'Groppa',
+    subtitle: 'HTML - Tailwind',
     github: 'https://github.com',
     demo: 'https://github.com/judsonkerller'
   },
   {
     id: 2,
     image: IMG2,
-    title: 'Linktree',
-    github: 'https://github.com',
-    demo: 'https://github.com/judsonkerller'
+    title: 'Plant Manager - App',
+    subtitle: 'React Native - Expo',
+    github: 'https://github.com/judsonkerller/plantmanager/tree/main',
+    demo: 'https://github.com/judsonkerller/plantmanager/tree/main'
   },
   {
     id: 3,
     image: IMG3,
     title: 'Event Platform',
-    github: 'https://github.com',
-    demo: 'https://github.com/judsonkerller'
+    subtitle: 'React - Tailwind - Vercel - GraphQL',
+    github: 'https://github.com/judsonkerller/event-platform',
+    demo: 'https://event-platform-pied-zeta.vercel.app/'
   },
 ]
 
@@ -36,13 +39,14 @@ const Portfolio = () => {
 
       <div className="container portfolio__container">
         {
-          data.map(({id, image, title, github, demo}) => {
+          data.map(({id, image, title, subtitle, github, demo}) => {
             return (
               <article key={id} className="portfolio__item">
                 <div className="portfolio__item-image">
                   <img src={image} alt={title} />
                 </div>
                 <h3>{title}</h3>
+                <p>{subtitle}</p>
                 <div className="portfolio__item-cta">
                   <a 
                     href={github} 
