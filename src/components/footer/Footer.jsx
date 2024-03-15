9a9dc3dd73988a31813fd6b548cfbc0650a348aa
judsonkerller <judsonkerller@gmail.com>
2024-03-14 21:15:13 -0300

    chore: update footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,12 +9,12 @@ const Footer = () => {
       <a href="#" className='footer__logo'>JK</a>
 
       <ul className="permalinks">
-        <li><a href="#"></a></li>
+        {/* <li><a href="#"></a></li> */}
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
-        <li><a href="#services">Services</a></li>
+        {/* <li><a href="#services">Services</a></li> */}
         <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
+        {/* <li><a href="#testimonials">Testimonials</a></li> */}
         <li><a href="#contact">Contact</a></li>
       </ul>
 
